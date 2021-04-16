4087d33da499c9defdee3da4397258517bcba4ae
Ryley Taub <taubr10@gmail.com>
2021-04-16 16:16:40 -0400

    first commit

---
+++ src/Model/Entity/Article.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Model\Entity;
+
+use \Cake\ORM\Entity;
+
+class Article extends Entity {
+
+    protected $_accessible = [
+
+        '*' => true,
+
+        'id' => false, 
+
+        'slug' => false
+
+    ];
+
+}
---
+++ src/Model/Table/ArticlesTable.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Model\Table;
+use Cake\ORM\Table;
+use Cake\Utility\Text;
+use Cake\Event\EventInterface;
+
+
+class ArticlesTable extends Table{
+    public function initialize (array $config) : void{
+        $this->addBehavior('Timestamp');
+    }
+}
